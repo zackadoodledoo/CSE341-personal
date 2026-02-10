99ea979817f18a54bf7866724230201fcb52f5ef
zackadoodledoo <sin12014@byui.edu>
2026-02-10 13:21:19 -0800

    updates to test. connect db through vs code

--- server.js
+++ server.js
@@ -25,3 +25,36 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/recipes-api', (req, res) => {
+  res.json({ message: "Success!" });
+});
+
+let recipes = []; // Temporary data storage
+
+// GET: Retrieve all recipes
+app.get('/recipes-api', (req, res) => {
+  res.json({ 
+    success: true, 
+    data: recipes 
+  });
+});
+
+// POST: Add a new recipe
+app.post('/recipes-api', (req, res) => {
+  const newRecipe = req.body; // Extract data from the request body
+  
+  if (!newRecipe.name) {
+    return res.status(400).json({ error: "Recipe name is required" });
+  }
+
+  recipes.push(newRecipe); // Save the recipe to our list
+  res.status(201).json({ 
+    message: "Recipe added successfully!", 
+    recipe: newRecipe 
+  });
+});
