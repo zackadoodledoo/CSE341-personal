a4c50b57af52d25a44a4d2e153037ecfb707a0a2
zackadoodledoo <sin12014@byui.edu>
2026-02-10 10:39:45 -0800

    bugs and breaking

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
+const validation = require('../middleware/validate');
 
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
-router.post('/', contactsController.createContact);
+router.get('/', validation.saveContact, contactsController.createContact);
 
-router.put('/:id', contactsController.updateContact);
+router.get('/:id', validation.savedContact, contactsController.updateContact);
 
 router.delete('/:id', contactsController.deleteContact);
 
